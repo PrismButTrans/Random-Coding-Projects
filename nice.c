7fc58aaceb32f0f2b311bbfdaabecc85986ab93b
Autumn <58234186+PrismButTrans@users.noreply.github.com>
2020-01-29 19:38:02 -0700

    Create nice.c

---
+++ nice.c
@@ -0,0 +1,48 @@
+/* I present to you:
+learning how to program in C to make
+a stupid joke
+
+ to do:
+        add an if statement for when the value is not a integer number (e.g. a floating point number or a word
+*/
+
+#include <stdio.h>
+int main(void){
+        int number = 0;
+        printf("enter a number: ");
+        scanf("%d", &number);
+        if (number == 69) {
+
+         printf("\nââââ   ââââââ âââââââââââââââ\nâââââ  ââââââââââââââ
+                                                                                
+        }else if (number == 420){
+                printf("\n â
+                            â
+                             â
+                              â
+                                   âââ    â
+                                           â
+                                            â
+                                                   âââââââââââââââ     ââââ
+                                                                           â
+                                                                            â
+                                                                             ââââââ ââââ\nâââââââ
+                  ââââ   ââââââ
+                                   â â â â
+                                          ââââ   â    âââââ  âââ ââ ââââ\nââââ â
+                                                                               ââââââ   âââ  âââ
+                  â â â
+                       âââ ââââ      âââââ ââââ ââ ââââ\nââââââ  ââââ   ââââ
+                                                                            â
+                                                                             â
+                                                                              â
+                                                                               ââ   â
+     ââ   ââââ  â
+                     âââââ ââââ â  ââââ\nâââ  âââââââââââââ   ââââââââââââââââââââ   ââââ  ââââ â  ââ
+                     â
+                       \nâââââââââ âââ  âââ   âââââââ âââââââ ââ â   ââ    â ââ    ââââ\nâââ   â â â â  â â   ââ ââââ â â â â â  â    â â    â     â  â\n â    â   â â    â   â   â â â â â   â       â â  â          â\n â          â  â     â  â  â â       â  â    â            â   \n      â                  â                                     \n");
+        }else{
+                printf("Sod off and try a something else.\n\n");
+        }
+        return 0;
+}
